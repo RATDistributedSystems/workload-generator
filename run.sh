98a407278792944b384596c2a63e46630e7a5e18
asinha94 <asinha94@gmail.com>
2018-02-11 17:26:28 -0800

    added little script which runs workload

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### PARAMS
+file="1userWorkLoad"
+rate="100" # in ms
+
+# Little animation before we execute
+for i in `seq 5 -1 1`; do 
+    echo -en "\rExecuting $file in $i seconds"
+    sleep 1
+done 
+echo -e "Starting\n"
+
+go run workgen.go -f files/$file -r $rate
